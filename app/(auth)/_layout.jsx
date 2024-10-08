6418d3b8e917fdfdac5fcbd3513af0eb4c719fed
Kunal <kunalkhan370@gmail.com>
2024-10-09 01:26:07 +0530

    Day: 1 - Created Project, setup layouts, nativewind, expo router tabs etc.

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AuthLayput = () => {
+  return (
+    <View>
+      <Text>AuthLayput</Text>
+    </View>
+  )
+}
+
+export default AuthLayput
