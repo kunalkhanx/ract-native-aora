67beada2d8b6e499232560736215d6a5d107193b
Kunal <kunalkhan370@gmail.com>
2024-10-10 01:18:37 +0530

    Day 2: Desigining, Images, SafeAreaView, StatusBar, Inputs, Custom Components etc.

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
 import React from 'react'
+import { Stack } from 'expo-router'
 
 const AuthLayput = () => {
   return (
-    <View>
-      <Text>AuthLayput</Text>
-    </View>
+    <>
+    <Stack>
+      <Stack.Screen name="sign-in" options={{headerShown: false}} />
+      <Stack.Screen name="sign-up" options={{headerShown: false}} />
+    </Stack>
+    <StatusBar backgroundColor ="#161622" style={'light'} />
+    </>
   )
 }
 
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -30,7 +30,9 @@ const RootLayput = () => {
   return (
     <Stack>
         <Stack.Screen name="index" options={{headerShown: false}} />
-        <Stack.Screen name="profile" />
+        <Stack.Screen name="(auth)" options={{headerShown: false}} />
+        <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+        {/* <Stack.Screen name="/search/[query]" options={{headerShown: false}} /> */}
     </Stack>
   )
 }
